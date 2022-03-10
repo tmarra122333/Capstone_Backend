411ca74388caa4d7d60c7210207104bc9ce2e96c
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-10 11:37:45 -0500

    working on database

--- Models/Gear.js
+++ Models/Gear.js
@@ -9,11 +9,6 @@ const GearSchema = new mongoose.Schema({
         type: String,
         required: [true, 'please enter name of the author.']
     },
-    price: {
-        type: Number,
-        min: [0, 'you can not add a negative number'],
-        required: [true, 'price can not be empty'],
-    },
     image: {
         type: String,
         required: [true, "image can not be empty"],
